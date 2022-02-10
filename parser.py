1d61d3ab5814951cb1b0db6a02d4c88c725fbf23
genya <arhiopteryx@gmail.com>
2022-02-10 11:02:25 +0200

    Обновлено вывод

--- parser.py
+++ parser.py
@@ -20,6 +20,7 @@ for i in tokensExel.values.tolist():
 
 #проходимся по каждому токену
 for token in tokenList:
+    print('Аккаунт ' + str(tokenList.index(token)+1) + '/' + str(len(tokenList)+1))
     try:
         r = None
         cookie = {'session': '17ab96bd8ffbe8ca58a7865'}
@@ -39,6 +40,8 @@ for token in tokenList:
             #проходимся по заказам
             for order in ordersValue:
                 orderStatus = order.find('td', class_='order-status').find('span', class_='f-left').text.strip()
+                orderName = order.find('a',  class_= 'baobei-name').text.strip()
+                orderPrice = order.find('p',  class_= 'amount-num').text.strip()
 
                 trackCode = ''
 
@@ -51,7 +54,7 @@ for token in tokenList:
                     trackCode = soup2.find('td', class_='no').find('div').text.strip()
 
                 #добавляем данные в базу
-                dataList.append([token, orderStatus, trackCode, i])
+                dataList.append([token, orderStatus, trackCode, i, orderName, orderPrice])
                 i = i+1
     except:
         print('Токен не валид')
@@ -73,21 +76,27 @@ for lastData in lastDataList:
     for curData in dataList:
         if lastData[0] == curData[0] and lastData[3] == curData[3]:     #если совпадает токен и  идентификатор
             if lastData[1] != curData[1]:       #если статусы заказов отличаються
-                changedData.append([lastData[0], lastData[1], curData[1], curData[2]])
+                changedData.append([lastData[0], lastData[1], curData[1], curData[2], curData[4], curData[5]])
 
 #формируем таблицу всех данных
 dfToken = []
 dfValue = []
 dfTrack = []
+dfName = []
+dfPrice = []
 
 for curData in dataList:        
     dfToken.append(curData[0])
     dfValue.append(curData[1])
     dfTrack.append(curData[2])
+    dfName.append(curData[4])
+    dfPrice.append(curData[5])
 
 df = pd.DataFrame({'Токен': dfToken,
                    'Статус': dfValue,
-                   'Трек': dfTrack})
+                   'Трек': dfTrack,
+                   'Имя': dfName,
+                   'Цена': dfPrice})
 
 df.to_excel('AllData.xlsx', sheet_name='Данные', index=False)
 
@@ -96,17 +105,23 @@ dfToken = []
 dfOldValue = []
 dfNewValue = []
 dfTrack = []
+dfName = []
+dfPrice = []
 
 for change in changedData:        
     dfToken.append(change[0])
     dfOldValue.append(change[1])
     dfNewValue.append(change[2])
     dfTrack.append(change[3])
+    dfName.append(curData[4])
+    dfPrice.append(curData[5])
 
 df = pd.DataFrame({'Токен': dfToken,
                    'Старое': dfOldValue,
                    'Обнова': dfNewValue,
-                   'Трек': dfTrack})
+                   'Трек': dfTrack,
+                   'Имя': dfName,
+                   'Цена': dfPrice})
 
 df.to_excel('Changed.xlsx', sheet_name='Данные', index=False)
 
